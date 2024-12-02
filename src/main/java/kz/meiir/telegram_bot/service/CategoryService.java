67d3cbd2a239f51302fc0f40a9891cff20d1dca2
Meiir Akhmetov <meir.akhmetov@gmail.com>
2024-12-02 20:40:42 +0500

    все работает, теперь будем все менять для шаблона проектирования Command

--- src/main/java/kz/meiir/telegram_bot/service/CategoryService.java
+++ src/main/java/kz/meiir/telegram_bot/service/CategoryService.java
@@ -5,7 +5,6 @@ import kz.meiir.telegram_bot.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoryService {
