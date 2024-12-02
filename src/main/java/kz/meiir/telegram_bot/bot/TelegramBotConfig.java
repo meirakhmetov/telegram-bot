16be7606ffe4130be1ac191e56c8398fbe36aa9d
Meiir Akhmetov <meir.akhmetov@gmail.com>
2024-12-02 10:15:33 +0500

    Создал model класс Category

--- src/main/java/kz/meiir/telegram_bot/bot/TelegramBotConfig.java
+++ src/main/java/kz/meiir/telegram_bot/bot/TelegramBotConfig.java
@@ -1,3 +1,30 @@
 package kz.meiir.telegram_bot.bot;
 
+import lombok.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBotConfig extends TelegramLongPollingBot {
+    @Value("${telegram.bot.username}")
+    private String botUsername;
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // Обработка команд
+    }
 }
---
+++ src/main/java/kz/meiir/telegram_bot/model/Category.java
@@ -0,0 +1,28 @@
+package kz.meiir.telegram_bot.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> children = new ArrayList<>();
+}
